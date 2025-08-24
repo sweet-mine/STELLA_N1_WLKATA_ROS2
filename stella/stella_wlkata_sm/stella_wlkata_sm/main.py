83358ca951e74734b8d8554c1a6e8dc502471afe
LSH <dltlgos@naver.com>
2025-08-24 21:55:41 +0900

    최신화

--- stella/stella_wlkata_sm/stella_wlkata_sm/main.py
+++ stella/stella_wlkata_sm/stella_wlkata_sm/main.py
@@ -6,6 +6,7 @@ from std_msgs.msg import String, Bool
 import struct
 import mmap
 import os
+import threading
 
 class ShmWriter:
     def __init__(self, path, size):
@@ -13,24 +14,30 @@ class ShmWriter:
         os.ftruncate(self.fd, size)
         self.mmap = mmap.mmap(self.fd, size, access=mmap.ACCESS_WRITE)
         self.size = size
+        self.version = 0
+        self.lock = threading.Lock()
 
     def write(self, data: bytes):
-        self.mmap.seek(0)
-        self.mmap.write(data.ljust(self.size, b'\x00'))  # padding to fixed size
+        with self.lock:
+            self.version = (self.version + 1) % (2**32)
+            self.mmap.seek(0)
+            self.mmap.write(struct.pack('I', self.version))  # write version (4 bytes)
+            self.mmap.write(data.ljust(self.size - 4, b'\x00'))  # padding
 
     def close(self):
         self.mmap.close()
         os.close(self.fd)
 
+
 class ROS2SharedMemoryBridge(Node):
     def __init__(self):
         super().__init__('stella_wlkata_node')
 
-        self.pose_shm = ShmWriter('/dev/shm/ros_bridge_pose', 32)
+        self.pose_shm = ShmWriter('/dev/shm/ros_bridge_pose', 64)
         self.js_shm = ShmWriter('/dev/shm/ros_bridge_jointstate', 256)
         self.direct_shm = ShmWriter('/dev/shm/ros_bridge_direct', 256)
-        self.gripper_shm = ShmWriter('/dev/shm/ros_bridge_gripper', 1)
-        self.homing_shm = ShmWriter('/dev/shm/ros_bridge_homing', 1)
+        self.gripper_shm = ShmWriter('/dev/shm/ros_bridge_gripper', 10)
+        self.homing_shm = ShmWriter('/dev/shm/ros_bridge_homing', 10)
 
         self.create_subscription(Pose, '/pose', self.pose_cb, 10)
         self.create_subscription(JointState, '/jointstate', self.js_cb, 10)
@@ -39,8 +46,17 @@ class ROS2SharedMemoryBridge(Node):
         self.create_subscription(Bool, '/homing', self.homing_cb, 10)
 
     def pose_cb(self, msg):
-        data = struct.pack('fff', msg.position.x, msg.position.y, msg.position.z)
+        data = struct.pack('7f',
+            msg.position.x,
+            msg.position.y,
+            msg.position.z,
+            msg.orientation.x,
+            msg.orientation.y,
+            msg.orientation.z,
+            msg.orientation.w
+        )
         self.pose_shm.write(data)
+
 
     def js_cb(self, msg):
         # For simplicity, store only first 6 joint positions
